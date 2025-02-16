46930c6056993c18119ed87dc3bb272a343e6f88
Dyno Nguyen <tuannguyentn2504@gmail.com>
2025-02-16 18:43:09 +0700

    🚀 deploy(build): Change chunk size limit warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,31 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, PluginOption } from 'vite'
-import { ViteEjsPlugin } from 'vite-plugin-ejs'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, PluginOption } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-const BASE_URL = process.env.VITE_BASE_URL || ''
+const BASE_URL = process.env.VITE_BASE_URL || '';
 
 const injectData = {
 	baseUrl: BASE_URL,
 	appName: 'Ezbill',
 	favicon: BASE_URL + '/img/logo.svg',
-}
+};
 
-const plugins: PluginOption[] = [vue(), ViteEjsPlugin(injectData), tailwindcss()]
+const plugins: PluginOption[] = [vue(), ViteEjsPlugin(injectData), tailwindcss()];
 
 if (process.env.ENABLE_VUE_DEVTOOL === 'true') {
-	plugins.push(vueDevTools())
+	plugins.push(vueDevTools());
 }
 
 export default defineConfig({
 	plugins,
 	envPrefix: 'VITE_',
 	base: BASE_URL,
-	resolve: {
-		alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
-	},
+	resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+	build: { chunkSizeWarningLimit: 1024 },
 	server: { open: true, port: 8888, host: '0.0.0.0' },
-})
+});
