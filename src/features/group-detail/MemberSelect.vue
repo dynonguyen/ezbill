216aa2f4182a8cb079ae915c7bc47c9174d2d380
Dyno Nguyen <tuannguyentn2504@gmail.com>
2025-05-15 17:00:23 +0700

    ✨ feat: Implement pagination for bill list and toggle view all functionality

--- src/features/group-detail/MemberSelect.vue
+++ src/features/group-detail/MemberSelect.vue
@@ -5,12 +5,15 @@ import Flex from '@/components/ui/Flex.vue';
 import type { Bill } from '@/types/entities';
 import { computed } from 'vue';
 import { useGroupContext } from './hooks/useGroupContext';
+import AccountingIcon from './members/AccountingIcon.vue';
 
 const { group } = useGroupContext();
 const value = defineModel<Bill['createdBy'] | null>('value');
 
 const createdByOptions = computed(() => {
-	return group.value.members.map((member) => ({ ...member, value: member.id }));
+	return group.value.members
+		.map((member) => ({ ...member, value: member.id }))
+		.sort((a, b) => (a.isAccounting || b.isAccounting ? -1 : 1));
 });
 </script>
 
@@ -20,6 +23,7 @@ const createdByOptions = computed(() => {
 			<Flex class="gap-2 w-full">
 				<MemberAvatar v-bind="option" size="sm" />
 				<span>{{ option.name }}</span>
+				<AccountingIcon v-if="option.isAccounting" />
 			</Flex>
 		</template>
 	</Autocomplete>
