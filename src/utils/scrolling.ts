2e7a8a8432026e7d12623d93287373d443499af5
“thngph“ <ngophuocthinh28@gmail.com>
2025-06-01 12:05:06 +0700

    🐞 fix(ui): Ios scroll behavior

--- src/utils/scrolling.ts
+++ src/utils/scrolling.ts
@@ -9,19 +9,25 @@ export const getScrollbarWidth = () => {
 	scrollDiv.style.overflow = 'scroll';
 	scrollDiv.style.position = 'absolute';
 	scrollDiv.style.top = '-9999px';
+
 	document.body.appendChild(scrollDiv);
 	const scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
 	document.body.removeChild(scrollDiv);
+
 	return scrollbarWidth;
 };
 
 export const lockScroll = () => {
-	document.body.style.overflow = 'hidden';
-	document.body.style.touchAction = 'none';
+	const body = document.body;
+	if (body.classList.contains('is-fixed')) return;
+
+	const scrollTop = window.scrollY;
+	body.classList.add('is-fixed');
+	body.style.top = `-${scrollTop}px`;
 	const scrollbarWidth = getScrollbarWidth();
 
 	if (hasScrollbar()) {
-		document.body.style.paddingRight = `${scrollbarWidth}px`;
+		body.style.paddingRight = `${scrollbarWidth}px`;
 		const stickyStatistic = document.getElementById('sticky-statistic');
 		if (stickyStatistic) {
 			stickyStatistic.style.left = `calc(50% - ${scrollbarWidth / 2}px)`;
@@ -30,11 +36,19 @@ export const lockScroll = () => {
 };
 
 export const unlockScroll = () => {
-	document.body.style.overflow = '';
-	document.body.style.touchAction = '';
-	document.body.style.paddingRight = '';
+	const body = document.body;
+
+	if (!body.classList.contains('is-fixed')) return;
+
+	const scrollTop = -parseInt(body.style.top || '0', 10) || 0;
+	body.classList.remove('is-fixed');
+	body.style.top = '';
+	body.style.paddingRight = '';
+
 	const stickyStatistic = document.getElementById('sticky-statistic');
 	if (stickyStatistic) {
 		stickyStatistic.style.left = '50%';
 	}
+
+	window.scrollTo(0, scrollTop);
 };
