484715bcc636497df114b399dcbf850a07e35453
Ngô Phước Thịnh <ngophuocthinh28@gmail.com>
2025-10-11 00:08:59 +0700

    ✨ feat: Add new banks to BANKS constant and implement buildVietQRUrl function for QR code generation (#18)
    * ✨ feat: Add new banks to BANKS constant and implement buildVietQRUrl function for QR code generation
    
    * ✨ feat: update transfer popup layout
    
    * 💥 update: Update imageUrlToBase64 return type

--- src/features/group-detail/balances/BankQR.vue
+++ src/features/group-detail/balances/BankQR.vue
@@ -5,7 +5,7 @@ import Flex from '@/components/ui/Flex.vue';
 import { useToast } from '@/hooks/useToast';
 import type { Member, MemberBankInfo } from '@/types/entities';
 import { saveFileAs } from '@/utils/helpers';
-import { buildVietQRData } from '@/utils/vietqr';
+import { buildVietQRData, buildVietQRUrl } from '@/utils/vietqr';
 import { useMutation } from '@tanstack/vue-query';
 import to from 'await-to-js';
 import { ref, watch } from 'vue';
@@ -50,19 +50,51 @@ const downloadQR = () => {
 	saveFileAs(qrBase64.value, `QR-thanh-toan-${props.bankInfo?.accountNumber}.jpeg`);
 };
 
+const imageUrlToBase64 = async (url: string): Promise<string> => {
+	const response = await fetch(url);
+	if (!response.ok)
+		throw new Error(`Failed to fetch image: ${response.status} ${response.statusText}`);
+
+	const blob = await response.blob();
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			if (typeof reader.result === 'string') resolve(reader.result);
+			else reject(new Error('Failed to convert image to base64 string.'));
+		};
+		reader.onerror = reject;
+		reader.readAsDataURL(blob);
+	});
+};
+
 watch(
 	() => [props.bankInfo, props.amount],
 	async () => {
 		if (props.bankInfo) {
-			const qrData = buildVietQRData({
-				accountNumber: props.bankInfo.accountNumber,
-				bin: props.bankInfo.bin,
+			const url = buildVietQRUrl({
+				accountNumber: props.bankInfo?.accountNumber,
+				bin: props.bankInfo?.bin,
 				amount: Number(props.amount.toFixed(0)),
 			});
 
-			import('qrcode').then(async (QRCode) => {
-				qrBase64.value = await QRCode.toDataURL(qrData, { width: 500, type: 'image/jpeg' });
-			});
+			const [err, base64] = await to(imageUrlToBase64(url));
+
+			if (!err && base64) {
+				qrBase64.value = base64;
+			} else {
+				const qrData = buildVietQRData({
+					accountNumber: props.bankInfo.accountNumber,
+					bin: props.bankInfo.bin,
+					amount: Number(props.amount.toFixed(0)),
+				});
+
+				import('qrcode').then(async (QRCode) => {
+					const [err, qrCode] = await to(
+						QRCode.toDataURL(qrData, { width: 500, type: 'image/jpeg' }),
+					);
+					if (!err && qrCode) qrBase64.value = qrCode;
+				});
+			}
 		}
 	},
 	{ immediate: true },
@@ -70,8 +102,8 @@ watch(
 </script>
 
 <template>
-	<Flex v-if="bankInfo" stack class="w-full">
-		<BankInfoDetail :bank-info="bankInfo" :amount="amount" :recipient="member.name">
+	<Flex v-if="bankInfo" stack class="w-full gap-3">
+		<BankInfoDetail :bank-info="bankInfo" :amount="amount" :recipient="member">
 			<template #action>
 				<Flex class="gap-2">
 					<span
@@ -83,7 +115,8 @@ watch(
 				</Flex>
 			</template>
 		</BankInfoDetail>
-		<img v-if="qrBase64" :src="qrBase64" class="size-44 mx-auto" />
+		<img v-if="qrBase64" :src="qrBase64" class="size-48 mx-auto" />
+		<div v-else class="skeleton size-48 mx-auto rounded-lg"></div>
 		<Flex center class="w-full">
 			<Button
 				variant="link"
